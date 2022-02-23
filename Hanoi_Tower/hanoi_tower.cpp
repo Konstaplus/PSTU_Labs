bb7f63501904e793174602681c39edaa0f77c705
Konstaplus <74364257+Konstaplus@users.noreply.github.com>
2022-02-23 21:27:17 +0500

    Hanoi_Towet

---
+++ Hanoi_Tower/hanoi_tower.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+void hanoi_towers(int quantity, int from, int to, int buf_peg) // 1: Кол-во дисков. 2: Из какого столбика 3: Куда 4 : Буферный столбик
+{
+if (quantity != 0)
+{
+	hanoi_towers(quantity - 1, from, buf_peg, to);
+	cout << from << " -> " << to << endl;
+	hanoi_towers(quantity - 1, buf_peg, to, from);
+}
+}
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int start_peg, destination_peg, buffer_peg, plate_quantity;
+	cout << "Номер первого столбика:" << endl;
+	cin >> start_peg;
+	cout << "Номер конечного столбика:" << endl;
+	cin >> destination_peg;
+	cout << "Номер промежуточного столбика:" << endl;
+	cin >> buffer_peg;
+	cout << "Количество дисков:" << endl;
+	cin >> plate_quantity;
+		hanoi_towers(plate_quantity, start_peg, destination_peg, buffer_peg);
+	return 0;
+}
