554abc1a36e2bba1fc8b1c611cb09972c3249cf7
Konstaplus <74364257+Konstaplus@users.noreply.github.com>
2022-03-09 21:15:10 +0500

    Add files via upload

---
+++ Labs 11/Labs_11-1.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+struct ELEM {
+    int value;
+    ELEM* pNext;
+}*head;
+
+void AddItem(int m_val)
+{
+    ELEM* pElem = new ELEM;
+    pElem->value = m_val;
+    pElem->pNext = head;
+    head = pElem;
+}
+
+void DeleteItems()
+{
+    ELEM* elem = head;
+    ELEM* prev = NULL;
+
+    while (elem)
+    {
+        if (elem->value % 2 == 0)
+        {
+            if (prev)
+            {
+                prev->pNext = elem->pNext;
+                elem = elem->pNext;
+                continue;
+            }
+            else
+                head = elem->pNext;
+        }
+        prev = elem;
+        elem = elem->pNext;
+    }
+}
+
+void PrintList()
+{
+    ELEM* elem = head;
+    while (elem)
+    {
+        printf("%d\n", elem->value);
+        elem = elem->pNext;
+    }
+}
+
+int main()
+{
+    for (int i = 0; i < 10; i++)
+        AddItem(i);
+
+    DeleteItems();
+    PrintList();
+
+    return 0;
+}
