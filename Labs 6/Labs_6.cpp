9a921ff81d76e73c2b42d79b1f4596514350c128
Konstaplus <74364257+Konstaplus@users.noreply.github.com>
2022-03-17 18:49:30 +0500

    Add files via upload

---
+++ Labs 6/Labs_6.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void main()
+{
+    cout << setiosflags(ios::left);
+    setlocale(LC_ALL, "rus");
+    string a = "Париж Москва Токио";
+    cout  << a << '\n';
+    int p, start = 0, value;
+    value = a.size();
+    for (int i = 0; i < value; i++)
+    {
+        if (a[i] == ' ')
+        {
+            p = i;
+            p = p - 1;
+            for (int ii = start; ii < p - ((p - start) / 2) + 1; ii++)
+            {
+                swap(a[ii], a[p]);
+                p--;
+            }
+            start = i+1; 
+        }
+    }
+    p = value-1;
+    for (int ii = start; ii < p - ((p - start) / 2) + 1; ii++)
+    {
+        swap(a[ii], a[p]);
+        p--;
+    }    
+    cout <<'\n' << a;    
+}
